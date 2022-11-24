5a8bcb428b0ee4d4444fc5e2a0b93af81a8014a2
JaRoD41 <laurent.trinco@gmail.com>
2022-11-24 22:15:14 +0100

    add stack logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
 
 import './App.css'
-import Home from './pages/home'
+import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
