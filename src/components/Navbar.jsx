7d27aa65dc2d2f18913fd87439107a28c3fd75ab
JaRoD41 <laurent.trinco@gmail.com>
2022-11-24 22:39:42 +0100

    navbar ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,16 @@ import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
 	return (
-		<Stack>
+		<Stack
+			direction="row"
+			justifyContent="space-around"
+			sx={{
+				gap: { sm: '122px', xs: '40px' },
+				mt: { sm: '32px', xs: '20px' },
+				justifyContent: 'none',
+			}}
+			px="20px"
+		>
 			<Link to="/">
 				<img
 					src={Logo}
@@ -12,7 +21,7 @@ const Navbar = () => {
 					style={{ width: '48px', height: '48px', margin: '0 20px' }}
 				/>
 			</Link>
-			<Stack>
+			<Stack direction="row" gap="40px" fontSize="24px" alignItems="center">
 				<Link
 					to="/"
 					style={{
