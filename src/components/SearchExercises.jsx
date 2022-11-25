9489f1b7bcce237b8ce1f735a8e3512e8e89e068
JaRoD41 <laurent.trinco@gmail.com>
2022-11-25 23:46:50 +0100

    add onClick to button

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import { SearchOffSharp } from '@mui/icons-material'
 
 const SearchExercises = () => {
+	const [search, setSearch] = useState('')
+
+	const handleSearch = async () => {
+		if (search) {
+			// const exercisesData = await fetchData()
+		}
+	}
+
 	return (
 		<Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
 			<Typography
@@ -24,8 +33,8 @@ const SearchExercises = () => {
 						borderRadius: '40px',
 					}}
 					height="76px"
-					value=""
-					onChange={(e) => {}}
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
 					placeholder="Search Exercises"
 					type="text"
 				/>
@@ -37,10 +46,11 @@ const SearchExercises = () => {
 						textTransform: 'none',
 						width: { lg: '175px', xs: '80px' },
 						fontSize: { lg: '20px', xs: '14px' },
-            height: '56px',
-            position: 'absolute',
-            right:'0'
+						height: '56px',
+						position: 'absolute',
+						right: '0',
 					}}
+					onClick={handleSearch}
 				>
 					Search
 				</Button>
