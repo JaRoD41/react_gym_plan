d990eb4ba327efa6bebe8b50d75b36f873b53398
JaRoD41 <laurent.trinco@gmail.com>
2022-12-01 19:12:05 +0100

    add 2 buttons

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -13,7 +13,7 @@ const ExerciseCard = ({exercise}) => {
           {exercise.target}
         </Button>
       </Stack>
-      <Typography>
+      <Typography ml="21px" color="#000" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize" fontSize="22px">
         {exercise.name}
       </Typography>
     </Link>
