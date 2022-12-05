4813056ead254a207d329f275869d4a566d8d725
JaRoD41 <laurent.trinco@gmail.com>
2022-12-05 10:26:27 +0100

    css fix

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -9,8 +9,8 @@ const LeftArrow = () => {
 	const { scrollPrev } = useContext(VisibilityContext)
 
 	return (
-		<Typography onClick={() => scrollPrev()} className="right-arrow">
-			<img src={LeftArrowIcon} alt="right-arrow" />
+		<Typography onClick={() => scrollPrev()} className="left-arrow">
+			<img src={LeftArrowIcon} alt="left-arrow" />
 		</Typography>
 	)
 }
@@ -19,7 +19,7 @@ const RightArrow = () => {
 	const { scrollNext } = useContext(VisibilityContext)
 
 	return (
-		<Typography onClick={() => scrollNext()} className="left-arrow">
+		<Typography onClick={() => scrollNext()} className="right-arrow">
 			<img src={RightArrowIcon} alt="right-arrow" />
 		</Typography>
 	)
